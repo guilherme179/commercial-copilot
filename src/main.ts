4bf32fe89155b94e6c126a2ee9ed5181288474e0
Guilherme Souza <guilherme.gui.souza179@gmail.com>
2026-04-17 20:34:39 -0300

    chore(app): configure global interceptors in main bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,18 @@ import 'dotenv/config';
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ErrorLoggingInterceptor } from './common/interceptors/error.interceptor';
+import { RequestLoggingInterceptor } from './common/interceptors/request-logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  app.useGlobalInterceptors(
+    new RequestLoggingInterceptor(),
+    new ErrorLoggingInterceptor(),
+  );
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
